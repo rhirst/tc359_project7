9df956a1624b47b5af84b9e58a70f2031990bdd3
rhirst <r.hirst18@gmail.com>
2015-02-16 19:39:43 -0500

    displays holidays from current month & birth month

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 # app.rb
-require 'sinatra'
 
-class MyWebApp < Sinatra::Base
-	get '/' do
-	last_modified Time.now
-  	erb :"index#{ rand(3) + 1}"
-	end
+require 'sinatra'
+require 'holidapi'
+
+class HolidaysApp < Sinatra::Base
+  get '/' do
+    @Holidata = HolidApi.get(country: 'us', year: Time.now.year, month: Time.now.month).flatten
+    @Bdata = HolidApi.get(year: 1992, month: 4).flatten
+    erb :"index#{ rand(3) + 1}"
+  end	
 end
+
+
