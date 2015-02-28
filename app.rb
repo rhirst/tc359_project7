435edb7aab0ac7db3595624a1688db5a95183194
rhirst <r.hirst18@gmail.com>
2015-02-28 15:16:02 -0500

    fixed indentation

--- app.rb
+++ app.rb
@@ -5,15 +5,15 @@ require 'holidapi'
 
 class HolidaysApp < Sinatra::Base
   get '/' do
-  	puts params
-  	params['holiday_country'] ||= 'us'
-  	params['holiday_year'] ||= Time.now.year 
-  	if params['holiday_year'] == ""
-  		params['holiday_year'] = Time.now.year
-  	end
+    puts params
+    params['holiday_country'] ||= 'us'
+    params['holiday_year'] ||= Time.now.year 
+    if params['holiday_year'] == ""
+      params['holiday_year'] = Time.now.year
+    end
   	
-  	params['holiday_month'] ||= 1
-  	puts params
+    params['holiday_month'] ||= 1
+    puts params
     @Holidata = HolidApi.get(country: params['holiday_country'], year: params['holiday_year'], month: params['holiday_month']).flatten
     @Bdata = HolidApi.get(year: 1992, month: 4).flatten
     erb :"index#{ rand(3) + 1}"
