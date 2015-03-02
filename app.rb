df0dff3d58e57ddfa781ba9756ffb155eef7e467
rhirst <r.hirst18@gmail.com>
2015-03-02 15:06:39 -0500

    indentation fix

--- app.rb
+++ app.rb
@@ -11,13 +11,12 @@ class HolidaysApp < Sinatra::Base
     if params['holiday_year'] == ""
       params['holiday_year'] = Time.now.year
     end
-  	
     params['holiday_month'] ||= 1
     puts params
     @Holidata = HolidApi.get(country: params['holiday_country'], year: params['holiday_year'], month: params['holiday_month']).flatten
     @Bdata = HolidApi.get(year: 1992, month: 4).flatten
     erb :"index#{ rand(3) + 1}"
-  end	
+  end 
 end
 
 
